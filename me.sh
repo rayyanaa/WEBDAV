b8ad25b60e1fe8049e36b4d5db3246b0de76a6e0
rayyanaa <47921394+rayyanaa@users.noreply.github.com>
2019-04-05 16:49:31 +0700

    Create me.sh

---
+++ me.sh
@@ -0,0 +1,21 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+echo
+clear
+echo
+echo $pur"=============≠====" $cy "======================"
+echo $i"#" $pu"  AUTHOR $me":"  $ku"K4MBING H1T4M" $i"  #"
+echo $i"#" $pu"  GMAIL  :  rayyanalifi69@gmail.com"  $i"#"
+echo $i"#" $pu"  Github :https://github.com/rayyanaa"$i"#"
+echo $i"#" $pu"  TEAM   :  Indonesia Security Team"  $i"#"
+echo $i"#" $pu"  TEAM   :  $ku"2Easy4Hack" $i"          #"
+echo $pur"=============≠====" $cy "======================"
+sleep 1
